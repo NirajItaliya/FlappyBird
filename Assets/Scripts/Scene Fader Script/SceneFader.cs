3a9641afe368987b8b4429acffa634a7faab1964
nirajtaliya123 <69964346+nirajtaliya123@users.noreply.github.com>
2021-02-12 11:54:39 +0530

    Commit

---
+++ Assets/Scripts/Scene Fader Script/SceneFader.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneFader : MonoBehaviour
+{
+	public static SceneFader instance;
+
+	[SerializeField]
+	private GameObject fadeCanvas;
+
+	[SerializeField]
+	private Animator fadeAnim;
+
+	void Awake()
+	{
+		MakeSingleton();
+	}
+
+	void MakeSingleton()
+	{
+		if (instance != null)
+		{
+			Destroy(gameObject);
+		}
+		else
+		{
+			instance = this;
+			DontDestroyOnLoad(gameObject);
+		}
+	}
+
+    public void FadeIn(string levelName)
+    {
+        StartCoroutine(FadeInAnimation(levelName));
+    }
+
+    public void FadeOut()
+    {
+        StartCoroutine(FadeOutAnimation());
+    }
+
+    IEnumerator FadeInAnimation(string levelName)
+    {
+        fadeCanvas.SetActive(true);
+        fadeAnim.Play("FadeIn");
+        yield return StartCoroutine(MyCoroutine.WaitForRealSeconds(0.7f));
+        Application.LoadLevel(levelName);
+        FadeOut();
+    }
+
+    IEnumerator FadeOutAnimation()
+    {
+        fadeAnim.Play("FadeOut");
+         yield return StartCoroutine(MyCoroutine.WaitForRealSeconds(1f));
+        fadeCanvas.SetActive(false);
+    }
+
+    internal void FadeIn(object gamePlay)
+    {
+        throw new NotImplementedException();
+    }
+}
